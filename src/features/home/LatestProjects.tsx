ec2ba54758466d35f9ff1abe949b2e4d830c1ffd
Ekene Nwobodo <Nwobodoe71@gmail.com>
2023-05-10 08:47:03 +0100

    Integrate active collection to homepage

--- src/features/home/LatestProjects.tsx
+++ src/features/home/LatestProjects.tsx
@@ -1,21 +1,36 @@
+'use client';
+
+import { useGetCollectionsQuery } from '@/redux/services/userApi';
 import { CollectionCard } from './components/CollectionCard';
 
 export default function LatestPRojects() {
+  const {
+    data: collections,
+    error,
+    isLoading
+  } = useGetCollectionsQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{`Error: ${error}`}</div>;
+
+
   return (
     <section className="h-full w-full">
       <div className="my-[30px] mb-8 flex w-full items-center justify-between px-2">
         <h2 className="font-rob text-[45px]/[45px] font-bold text-white">Latest projects</h2>
       </div>
 
-      <div className="grid w-full grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
-        <CollectionCard image="/assets/project-buy.png" />
-        <CollectionCard image="/assets/project-buy.png" />
-        <CollectionCard image="/assets/project-buy.png" />
-        <CollectionCard image="/assets/project-buy.png" />
-        <CollectionCard image="/assets/project-buy.png" />
-        <CollectionCard image="/assets/project-buy.png" />
-        <CollectionCard image="/assets/project-buy.png" />
-        <CollectionCard image="/assets/project-buy.png" />
+      <div className="grid w-full grid-cols-2 justify-items-center gap-3 md:grid-cols-3 lg:grid-cols-4">
+        {collections?.data.map(collection => (
+          <CollectionCard
+            image={collection.avatar}
+            name={collection.name}
+            creator={collection.owner}
+            floor_price={collection.floor_price}
+            description={collection.description}
+            status={collection.status}
+          />
+        ))}
       </div>
     </section>
   );
--- src/features/home/components/CollectionCard.tsx
+++ src/features/home/components/CollectionCard.tsx
@@ -1,22 +1,32 @@
 import { Button } from '@/ui/Button';
 import Image from 'next/image';
 
-export const CollectionCard = ({ image }: { image: string }) => (
+export interface ICollections {
+  name: string;
+  description: string;
+  image: string;
+  creator: string;
+  floor_price: string;
+  status: string;
+}
+
+
+export const CollectionCard = ({ name, creator, description, status, floor_price, image }: ICollections) => (
   <div className="block h-full w-full rounded-[19px] bg-grey-200 p-4 font-rob">
     <Image src={image} alt="project" width={293} height={156} className="mb-[21px] w-full rounded-[16px]" />
 
-    <h1 className="truncate pb-2 font-rob text-base/[16px] font-bold text-white">Chromie Squiggle by Snowfro</h1>
+    <h1 className="truncate pb-2 font-rob text-base/[16px] font-bold text-white">{name}</h1>
 
-    <p className="font-rob text-[14px]/[14px] font-bold text-white text-opacity-60">By Art_Blocks</p>
+    <p className="font-rob text-[14px]/[14px] font-bold text-white text-opacity-60">{creator}</p>
 
     <div>
       <div>
-        <span className="text-[12px]/[12px] font-normal text-white">Collection</span>
-        <span className="text-[14px]/[14px] font-bold text-white">Yes</span>
+        <span className="text-[12px]/[12px] font-normal text-white">{description}</span>
+        <span className="text-[14px]/[14px] font-bold text-white">{status}</span>
       </div>
       <div className="flex w-full justify-end gap-4 py-[28px]">
         <span className="text-[12px]/[12px] font-normal text-white">Floor Price:</span>
-        <span className="text-[14px]/[14px] font-bold text-white">0.08 ETH</span>
+        <span className="text-[14px]/[14px] font-bold text-white">{`${floor_price} ETH`}</span>
       </div>
     </div>
 
