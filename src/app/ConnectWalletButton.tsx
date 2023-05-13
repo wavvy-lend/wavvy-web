f91828bdaed8fa19f5c3eeaf7dac591ac7a28c1c
izaakwalz <chukwumaizaak@gmail.com>
2023-05-13 14:54:33 +0100

    feat: add create user on connectwallet

--- src/app/ConnectWalletButton.tsx
+++ src/app/ConnectWalletButton.tsx
@@ -15,7 +15,7 @@ import { useContractContext } from '@/context/contract-context';
 // import { connectWallet } from '@/redux/services/account.servie';
 
 export default function ConnectWalletButton() {
-  const { account, connectWallet } = useContractContext();
+  const { account, connectWallet, disconnect } = useContractContext();
 
   const { address, isAuthenticated } = account;
 
@@ -38,7 +38,7 @@ export default function ConnectWalletButton() {
           <Popover className="relative">
             {({ open }) => (
               <>
-                <Popover.Button className="flex items-center justify-center gap-2.5 rounded-lg bg-alt-300 px-[29px] py-[15px] font-rob text-[14px]/[14px] font-bold text-white">
+                <Popover.Button className="flex items-center justify-center gap-2.5 rounded-lg bg-alt-300 px-[29px] py-[15px] font-rob text-[14px]/[14px] font-bold text-white outline-none">
                   <ArrowUturnRightIcon className="h-5 w-5 stroke-alt-500" />
                   {shortenAddress(address)}
                   <ChevronDownIcon className="h-[14px] w-[14px] stroke-grey-100" />
@@ -52,7 +52,7 @@ export default function ConnectWalletButton() {
                   leaveFrom="opacity-100 translate-y-0"
                   leaveTo="opacity-0 translate-y-1"
                 >
-                  <Popover.Panel className="absolute z-10 mt-3 flex w-[200px] flex-col items-center gap-4 overflow-hidden rounded-lg bg-alt-300 bg-opacity-5 shadow-lg backdrop-blur-[10px]  backdrop-filter  transition-opacity">
+                  <Popover.Panel className="absolute z-10 mt-3 flex w-[200px] flex-col items-center gap-4 overflow-hidden rounded-lg bg-grey-200 bg-opacity-10 shadow-lg backdrop-blur-[10px]  backdrop-filter  transition-opacity">
                     <div className="relative flex w-full flex-col gap-4 px-2 py-4">
                       <button className="flex w-full items-center justify-center gap-4 rounded-lg bg-alt-500 bg-opacity-20 p-2 text-[14px]/[14px] font-bold text-grey-100 hover:bg-opacity-50">
                         <span> {shortenAddress(address)}</span>
@@ -60,7 +60,10 @@ export default function ConnectWalletButton() {
                       </button>
                     </div>
                     <div className="w-full flex-col gap-4 px-2 py-4 first-letter:flex">
-                      <button className="flex w-full items-center justify-center gap-4 rounded-lg bg-alt-200 bg-opacity-20 p-2 text-[14px]/[14px] font-bold text-grey-100 hover:bg-opacity-50">
+                      <button
+                        className="flex w-full items-center justify-center gap-4 rounded-lg bg-alt-200 bg-opacity-20 p-2 text-[14px]/[14px] font-bold text-grey-100 hover:bg-opacity-50"
+                        onClick={disconnect}
+                      >
                         <span>Logout</span>
                         <ArrowRightOnRectangleIcon className="h-5 w-5" />
                       </button>
--- src/context/contract-context.tsx
+++ src/context/contract-context.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { useCreateUserMutation } from '@/redux/services/userApi';
 import { CHAIN_INFO, SupportedNetWork } from '@/util/chain';
-import { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { createContext, useCallback, useContext, useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
 
 declare global {
@@ -35,6 +36,8 @@ export interface ContractProps extends React.PropsWithChildren {}
 export default function ContractProvider({ children }: ContractProps) {
   const [account, setAccount] = useState<IAccount>(initialState.account);
 
+  const [createUser, { isLoading, error, data }] = useCreateUserMutation();
+
   const connectWallet = useCallback(async () => {
     try {
       const { ethereum } = window;
@@ -55,11 +58,39 @@ export default function ContractProvider({ children }: ContractProps) {
         isAuthenticated: true
       });
 
+      if (error) return console.log(error);
+
+      await createUser({ address: accounts[0] });
+
+      if (!isLoading) return;
+
+      if (data) {
+        toast.error('User Successfully registered');
+        return;
+      }
+
       localStorage.setItem('chain_network', chainNetwork);
     } catch (error) {
       console.log(error);
     }
   }, [setAccount]);
+
+  const disconnect = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        toast.error('No ethereum wallet found, Please install metamask');
+        return;
+      }
+
+      const accounts: any = (await ethereum.request({ method: 'eth_accounts' })) as string[];
+      if (accounts?.length > 0) {
+        setAccount(initialState.account);
+      }
+      localStorage.removeItem('chain_network');
+    } catch {}
+  };
 
   const confirmWalletConnection = useCallback(async () => {
     try {
@@ -111,11 +142,11 @@ export default function ContractProvider({ children }: ContractProps) {
 
   console.log(account);
 
-  const contextValue = useMemo(() => {
-    return { account, connectWallet };
-  }, [account, connectWallet]);
+  // const contextValue = useMemo(() => {
+  //   return { account, connectWallet };
+  // }, [account, connectWallet]);
 
-  return <ContractContext.Provider value={{ account, connectWallet }}>{children}</ContractContext.Provider>;
+  return <ContractContext.Provider value={{ account, connectWallet, disconnect }}>{children}</ContractContext.Provider>;
 }
 
 export const useContractContext = () => useContext(ContractContext);
