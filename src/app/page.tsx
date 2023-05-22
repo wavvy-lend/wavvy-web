fc392bdbb2018e587e13fa221ea0ba82b12ec4a6
Ekene Nwobodo <Nwobodoe71@gmail.com>
2023-05-22 23:07:31 +0100

    Fix conflict
    Signed-off-by: Ekene Nwobodo <Nwobodoe71@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import Hero from '@/features/home/Hero';
 import LatestPRojects from '@/features/home/LatestProjects';
-<<<<<<< HEAD
-import LendableProjects from '@/features/home/LendableProjects';
-import { Button } from '@/ui/Button';
-import Image from 'next/image';
-=======
 import LendableProjects from '@/features/home/LendableProjects';;
->>>>>>> f9414fc (Add item search)
 
 export default function Home() {
   return (
