02e84bf32243d5920b96148e273206e7339aeb32
Ekene Nwobodo <Nwobodoe71@gmail.com>
2023-06-08 10:07:02 +0100

    Fix console error
    Signed-off-by: Ekene Nwobodo <Nwobodoe71@gmail.com>

--- next.config.js
+++ next.config.js
@@ -8,14 +8,12 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
-
-// module.exports = {
-//   ...nextConfig,
-//   webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-//     if (isServer) {
-//       config.externals.push({ bufferutil: 'bufferutil', 'utf-8-validate': 'utf-8-validate' });
-//     }
-//     return config;
-//   }
-// };
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    if (isServer) {
+      config.externals.push({ bufferutil: 'bufferutil', 'utf-8-validate': 'utf-8-validate' });
+    }
+    return config;
+  }
+};
