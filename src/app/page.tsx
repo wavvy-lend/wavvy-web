cf21a6e2a5ffa85a0d90d3459c3f6559bccaa634
okorieebube <okorieebube1@gmail.com>
2023-06-11 02:15:53 +0100

    edited how it works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <HowItworks />
       <LatestPRojects />
+      <HowItworks />
       {/* <LendableProjects /> */}
     </>
   );
--- src/features/home/how-it-works.tsx
+++ src/features/home/how-it-works.tsx
@@ -2,43 +2,46 @@ import Image from 'next/image';
 
 const HowItworks = () => {
   return (
-    <section className="h-full w-full rounded-3xl bg-grey-200 p-6">
+    <section className="h-full w-full rounded-3xl bg-grey-200 p-6 mt-6">
       <h1 className="py-[60px] text-center text-[45px]/[45px] font-bold text-white">How it works</h1>
-      <div className="flex w-full items-start gap-4">
+      <div className="flex w-full items-start gap-4 pb-6">
         <Image src={'/assets/nft-banner.png'} alt="How it works" width={450} height={422} className="" priority />
 
         <div className="grid grid-cols-2 gap-3">
           <StepFeature
             id="01"
             label="Connect Your Wallet"
-            description="Connect Your Wallet
-            Connecting your wallet gives WAVVY access to set up the platform to your needs."
+            description="This sets up your account and is required to perform any action on Wavvy."
           />
           <StepFeature
             id="02"
-            label="Select NFT Collection"
-            description="Below are NFT collections that we curently supported by our platform and are eligible for loans."
+            label="Choose an NFT Collection"
+            description="Above are NFT collections that we curently support and are eligible for loans on Polygon."
           />
           <StepFeature
             id="03"
-            label="Search by NFT token ID"
-            description="Make a direct search to the NFT token you want and if its available you can make a purchase."
+            label="Search for NFT Token ID"
+            description="Search for the NFT token of your choice, and if its listed for sale you can make a purchase instantly."
           />
           <StepFeature
             id="04"
-            label="Get Loan from pool"
-            description="You will provided with diffrent flexible loan terms to pick from the."
+            label="Select a Pool to Fund Purchase"
+            description="You will be provided with different flexible loan terms by lenders to fund your purchase."
           />
+          <StepFeature
+          id="06"
+          label="Complete Purchase"
+          description="Once you've settled on a loan term, just click on buy and Wavvy will make the purchase. If purchase was successfull, visit your dashboard to view NFT"
+        />
         </div>
       </div>
-      <div className="flex w-full items-center gap-3 px-4">
+      {/* <div className="flex w-full items-center gap-3 px-4">
         <StepFeature
           id="05"
           label="Start loan plan"
           description="Once you've settled on a loan term and decided to borrow, it's a matter of a click away we will make the purchase for you directly."
         />
-        {/* <StepFeature id="06" label="" description="" /> */}
-      </div>
+      </div> */}
     </section>
   );
 };
